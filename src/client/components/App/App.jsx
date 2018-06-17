975bc2b8021ebef5678354c9ce4988157227104d
Steven Zhang <stevenzhang717@gmail.com>
2018-06-17 17:15:38 +1000

    code spliting

--- src/client/components/App/App.jsx
+++ src/client/components/App/App.jsx
@@ -5,14 +5,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Navigation from '../Shared/Navigation';
-import Posts from '../Posts/Posts';
-import Home from '../Home';
-import EditPostForm from '../EditPostForm';
 import reducers from '../../reducers';
-import SignIn from '../SignIn/Signin';
-import Signup from '../Signup/Signup';
 import SessionInitializer from '../StateManagementComponent/SessionInitializer';
 import './App.css';
+import AsyncComponent from './AsyncComponent';
+
+const Home = AsyncComponent(() => import('../Home').then(module => module.default));
+const Posts = AsyncComponent(() => import('../Posts/Posts').then(module => module.default));
+const EditPostForm = AsyncComponent(() => import('../EditPostForm').then(module => module.default));
+const SignIn = AsyncComponent(() => import('../SignIn/Signin').then(module => module.default));
+const Siginup = AsyncComponent(() => import('../Signup/Signup').then(module => module.default));
 
 const App = () => (
   <Provider
@@ -31,7 +33,7 @@ const App = () => (
           <Route exact path="/posts" component={Posts} />
           <Route exact path="/posts/create" component={EditPostForm} />
           <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/signup" component={Siginup} />
         </div>
       </div>
     </Router>
